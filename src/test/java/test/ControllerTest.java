458afa69d55c6967fb9a81ac43ccbc3398d8c705
Max Ziegerer <max.ziegerer@smail.inf.h-brs.de>
2023-10-24 02:24:13 +0200

    lots of edits to test class to where it works similar to my implementation because couldnt get the given one to work.

--- src/test/java/test/ControllerTest.java
+++ src/test/java/test/ControllerTest.java
@@ -1,6 +1,7 @@
 package test;
 
-import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import de.hbrs.ia.model.SalesMan;
@@ -8,13 +9,14 @@ import org.bson.Document;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.mongodb.client.model.Filters.eq;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class HighPerformanceTest {
+class ControllerTest {
 
-    private MongoClient client;
-    private MongoDatabase supermongo;
-    private MongoCollection<Document> salesmen;
+    private MongoClient mongoClient;
+    private MongoDatabase personal;
+    private MongoCollection<Document> info;
 
     /**
      * Attention: You might update the version of the Driver
@@ -23,15 +25,10 @@ class HighPerformanceTest {
      */
     @BeforeEach
     void setUp() {
-        // Setting up the connection to a local MongoDB with standard port 27017
-        // must be started within a terminal with command 'mongod'.
-        client = new MongoClient("localhost", 27017);
-
-        // Get database 'highperformance' (creates one if not available)
-        supermongo = client.getDatabase("highperformance");
-
-        // Get Collection 'salesmen' (creates one if not available)
-        salesmen = supermongo.getCollection("salesmen");
+        String uri = "mongodb+srv://mziege2s:Max21.11.@cluster0.wqctpff.mongodb.net/?retryWrites=true&w=majority";
+        mongoClient = MongoClients.create(uri);
+        personal = mongoClient.getDatabase("personal");
+        info = personal.getCollection("test");
     }
 
     @Test
@@ -43,18 +40,22 @@ class HighPerformanceTest {
         document.append("id" , 90133);
 
         // ... now storing the object
-        salesmen.insertOne(document);
+        info.insertOne(document);
 
         // READ (Finding) the stored Documnent
-        Document newDocument = this.salesmen.find().first();
-        System.out.println("Printing the object (JSON): " + newDocument );
+        Document newDocument = info.find(eq("id", "90133")).first();
+        if (newDocument != null) {
+            System.out.println(newDocument.toJson());
+            // Assertion
+            Integer id = (Integer) newDocument.get("id");
+            assertEquals( 90133 , id );
+        } else {
+            System.out.println("No matching documents found.");
+        }
 
-        // Assertion
-        Integer id = (Integer) newDocument.get("id");
-        assertEquals( 90133 , id );
 
         // Deletion
-        salesmen.drop();
+        info.drop();
     }
 
     @Test
@@ -64,11 +65,11 @@ class HighPerformanceTest {
         SalesMan salesMan = new SalesMan( "Leslie" , "Malton" , 90444 );
 
         // ... now storing the object
-        salesmen.insertOne(salesMan.toDocument());
+        info.insertOne(salesMan.toDocument());
 
         // READ (Finding) the stored Documnent
         // Mapping Document to business object would be fine...
-        Document newDocument = this.salesmen.find().first();
+        Document newDocument = this.info.find().first();
         System.out.println("Printing the object (JSON): " + newDocument );
 
         // Assertion
@@ -76,6 +77,6 @@ class HighPerformanceTest {
         assertEquals( 90444 , id );
 
         // Deletion
-        salesmen.drop();
+        info.drop();
     }
 }
